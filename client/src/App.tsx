94714efe1050edb9a5395b6afde34a0dfc4139a5
Rahul Hegde <rahulh.hegde@gmail.com>
2023-05-11 22:39:40 -0700

    [feat] added basic socket connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { io } from "socket.io-client";
 
 function App() {
-	return <div className="App"></div>;
+	const socket = io("http://localhost:8080");
+	socket.on("connect", () => {
+		console.log(socket.id);
+	});
+
+	socket.on("disconnect", () => {
+		console.log(socket.id);
+	});
+
+	const handlePress = () => {
+		socket.emit("newgame", "player1");
+	};
+	return (
+		<div className="App">
+			<button onClick={handlePress} />
+		</div>
+	);
 }
 
 export default App;
--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,24 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
+const port = process.env.PORT || 8080;
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:3000",
+	},
+});
+
+io.on("connection", (socket) => {
+	console.log("connected");
+	socket.on("newgame", (arg) => {
+		console.log(arg);
+	});
+	socket.on("disconnect", () => {
+		console.log("disconnected");
+	});
+});
+
+server.listen(port);
