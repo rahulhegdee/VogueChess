8c409a8620f1d5e15ffc19d2597970b6be1a9211
Rahul Hegde <rahulh.hegde@gmail.com>
2023-08-27 23:07:05 -0700

    [cleanup] comment clean up

--- client/src/Menu.tsx
+++ client/src/Menu.tsx
@@ -1,14 +1,4 @@
 function Menu() {
-	// useEffect(() => {
-	// 	if (isPlaying) {
-	// 		socket.emit("join_game", "ready");
-
-	// 		socket.on("game_status", (arg) => {
-	// 			console.log(arg);
-	// 		});
-	// 	}
-	// }, [isPlaying]);
-
 	return <button>Join Game!</button>;
 }
 
